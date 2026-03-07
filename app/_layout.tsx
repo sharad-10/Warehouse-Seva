5985a0109f96659c5a3a9b3f1ea69acaaec6f146
sharad-10 <singhaniashara24d@gmail.com>
2026-03-07 18:59:48 +0530

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+          <StatusBar style="dark" />
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
